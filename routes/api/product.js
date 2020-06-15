f9567a1fafaad44654890c78c6102048e628daf6
Eggy Bond <eggybond18@gmail.com>
2020-06-15 21:59:25 +0700

    change product detail, list, update

--- routes/api/product.js
+++ routes/api/product.js
@@ -9,6 +9,17 @@ const passport = require('passport');
  * @desc Get product detail
  * @access Public
  */
+
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'storas_backend',
+    password: 'root',
+    port: 5432,
+})
+
 router.get('/detail', async (req, res) => {
     const productId = req.query.id;
     const product = await Product.findByPk(productId);
@@ -19,22 +30,28 @@ router.get('/detail', async (req, res) => {
             errorMessage: "Product not found"
         })
     }
-    const city = await City.findByPk(product.cityId);
 
     return res.status(200).json({
         result: {
             id: product.id,
             name: product.name,
             description: product.description,
-            cityName: city.name,
+            city: product.city,
             warehouseType: product.warehouseType,
             images: product.images,
             price: product.price,
             geoLocation: {
                 lng: product.geoLng,
                 lat: product.geoLat
-            }
+            },
+            district: product.district,
+            building_area: product.building_area,
+            electricity: product.electricity,
+            total_floor: product.total_floor,
+            pdam: product.pdam,
+            additional_facility: product.additional_facility
         },
+        
         success: true,
         errorMessage: null
     });
@@ -46,10 +63,12 @@ router.get('/detail', async (req, res) => {
  * @desc Get product list
  * @access Public
  */
-router.get('/list', async (req, res) => {
+router.get('/list', passport.authenticate('jwt', {
+    session: false
+}),async (req, res) => {
     const warehouseType = req.query.warehouseType;
     const ownedOnly = req.query.ownedOnly || false;
-
+    console.log("Testt ",req.query)
     let whereQuery = {};
     if (warehouseType) {
         whereQuery['warehouseType'] = warehouseType;
@@ -59,26 +78,14 @@ router.get('/list', async (req, res) => {
         whereQuery['ownerId'] = user.id;
     }
 
+    whereQuery['deleted'] = false;
+    
     const productList = await Product.findAll({
         where: whereQuery
     });
 
     const productsResult = [];
 
-    // for (const product of productList) {
-    //     let city = await City.findByPk(product.cityId);
-
-    //     productsResult.push(
-    //         {
-    //             id: product.id,
-    //             name: product.name,
-    //             cityName: city.name,
-    //             warehouseType: product.warehouseType,
-    //             image: product.images[0],
-    //             price: product.price
-    //         }
-    //     )
-    // }
 
     return res.status(200).json({
         result: {
@@ -121,18 +128,10 @@ router.post('/upsert', passport.authenticate('jwt', {
         building_area,
         electricity,
         total_floor,
-        padam,
+        pdam,
         additional_facility
     } = req.body;
 
-    const Pool = require('pg').Pool
-    const pool = new Pool({
-        user: 'ubuntu',
-        host: 'localhost',
-        database: 'storas_backend',
-        password: 'root',
-        port: 5432,
-    })
 
     try {
         if (id) {
@@ -163,7 +162,7 @@ router.post('/upsert', passport.authenticate('jwt', {
             building_area,
             electricity,
             total_floor,
-            padam,
+            pdam,
             additional_facility
         ]
 
@@ -179,29 +178,7 @@ router.post('/upsert', passport.authenticate('jwt', {
             success: true,
             errorMessage: null
         });
-        // const [record, created] = await Product.upsert({
-        //         id,
-        //         ownerId: user.id,
-        //         name,
-        //         description,
-        //         warehouseType,
-        //         images,
-        //         price,
-        //         geoLng: geolocation.lng,
-        //         geoLat: geolocation.lat
-        //     },
-        //     {
-        //         returning: true
-        //     }
-        // );
 
-        // return res.status(201).json({
-        //     result: {
-        //         productId: record.id
-        //     },
-        //     success: true,
-        //     errorMessage: null
-        // });
     } catch (e) {
         return res.status(500).json({
             result: {
@@ -213,5 +190,165 @@ router.post('/upsert', passport.authenticate('jwt', {
     }
 
 });
+
+router.put('/delete', passport.authenticate('jwt', {
+    session: false
+}), async (req, res) => {
+
+    if (user.type !== "PRODUCT_OWNER") {
+        return res.status(403).json({
+            result: {
+                productId: null
+            },
+            success: false,
+            errorMessage: "Unauthorized. Only Product Owner can create product"
+        });
+    }
+
+    const {
+        id,
+    } = req.body;
+
+
+    try {
+        if (id) {
+            const existingProduct = await Product.findByPk(id);
+            if (existingProduct.ownerId !== user.id) {
+                return res.status(403).json({
+                    result: {
+                        productId: null
+                    },
+                    success: true,
+                    errorMessage: "Unauthorized. You're not the owner of this product."
+                });
+            }
+        }
+
+
+
+        Product.update(
+            {deleted: true},
+            {where: {id: id}}
+        ).then(function(rowsUpdated) {
+            return res.status(201).json({
+                success: true,
+                errorMessage: null
+            });
+        }).catch(function(e){
+            return res.status(400).json({
+                success: false,
+                errorMessage: e
+            });
+        })
+
+
+    } catch (e) {
+        return res.status(500).json({
+            result: {
+                productId: null
+            },
+            success: false,
+            errorMessage: e.message
+        });
+    }
+
+});
+
+router.put('/update', passport.authenticate('jwt', {
+    session: false
+}), async (req, res) => {
+
+    if (user.type !== "PRODUCT_OWNER") {
+        return res.status(403).json({
+            result: {
+                productId: null
+            },
+            success: false,
+            errorMessage: "Unauthorized. Only Product Owner can create product"
+        });
+    }
+
+    const {
+        id,
+        name,
+        description,
+        warehouseType,
+        images,
+        price,
+        geolocation,
+        city,
+        district,
+        building_area,
+        electricity,
+        total_floor,
+        pdam,
+        additional_facility
+    } = req.body;
+
+
+    try {
+        if (id) {
+            const existingProduct = await Product.findByPk(id);
+            if (existingProduct.ownerId !== user.id) {
+                return res.status(403).json({
+                    result: {
+                        productId: null
+                    },
+                    success: true,
+                    errorMessage: "Unauthorized. You're not the owner of this product."
+                });
+            }
+        }
+
+
+
+        var update_query = 'UPDATE public."Products" SET "name"=$1, "ownerId"=$2, "city"=$3, "warehouseType"=$4, images=$5, "geoLng"=$6, "geoLat"=$7, price=$8, "updatedAt"=NOW(), description=$9, building_area=$10, electricity=$11, total_floor=$12, pdam=$13, additional_facility=$14, district=$15 WHERE id=$16;';
+        var values = [
+            name,
+            user.id,
+            city,
+            warehouseType,
+            images,
+            geolocation.lng,
+            geolocation.lat,
+            price,
+            description,
+            building_area,
+            electricity,
+            total_floor,
+            pdam,
+            additional_facility,
+            district,
+            id
+        ]
+
+        pool.query(update_query, values, (error, results) => {
+            if (error) {
+                return res.status(400).json({
+                    success: false,
+                    errorMessage: error
+                });
+            }
+            console.log(results)
+            return res.status(201).json({
+                success: true,
+                errorMessage: null
+            });
+          })
+
+
+
+    } catch (e) {
+        return res.status(500).json({
+            result: {
+                productId: null
+            },
+            success: false,
+            errorMessage: e.message
+        });
+    }
+
+});
+
 
 module.exports = router;
--- services/queries.js
+++ services/queries.js
@@ -1,5 +1,5 @@
 const Pool = require('pg').Pool
-export const pool = new Pool({
+const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'storas_backend',
